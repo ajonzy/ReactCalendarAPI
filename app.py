a890cfafd76aafe5f03956b4f4f23053ebcbb150
Alex Jones <ajonzy@gmail.com>
2021-03-24 10:07:39 -0600

    API finished

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_heroku import Heroku
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = ""
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://mtafzsrnlfybdd:2693694e171752c6330b1133b6b1fc0e6fd144d6dfe9c0dc8014a2e9b3b15f6e@ec2-23-21-229-200.compute-1.amazonaws.com:5432/d9clhorui9ktrb"
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
@@ -52,6 +52,101 @@ class ReminderSchema(ma.Schema):
 
 reminder_schema = ReminderSchema()
 multiple_reminder_schema = ReminderSchema(many=True)
+
+
+@app.route("/month/add", methods=["POST"])
+def add_month():
+    if request.content_type != "application/json":
+        return jsonify("Error: Data must be sent as JSON.")
+
+    post_data = request.get_json()
+    name = post_data.get("name")
+    start_day = post_data.get("start_day")
+    days_in_month = post_data.get("days_in_month")
+    days_in_previous_month = post_data.get("days_in_previous_month")
+    year = post_data.get("year")
+
+    record = Month(name, start_day, days_in_month, days_in_previous_month, year)
+    db.session.add(record)
+    db.session.commit()
+
+    return jsonify("Month added")
+
+@app.route("/month/add/multiple", methods=["POST"])
+def add_multiple_months():
+    if request.content_type != "application/json":
+        return jsonify("Error: Data must be sent as JSON.")
+
+    post_data = request.get_json()
+    data = post_data.get("data")
+    for month in data:
+        record = Month(month["name"], month["start_day"], month["days_in_month"], month["days_in_previous_month"], month["year"])
+        db.session.add(record)
+
+    db.session.commit()
+
+    return jsonify("All months added")        
+
+@app.route("/month/get", methods=["GET"])
+def get_all_months():
+    all_months = db.session.query(Month).all()
+    return jsonify(multiple_month_schema.dump(all_months))
+
+@app.route("/month/get/<month_name>/<month_year>", methods=["GET"])
+def get_one_month(month_name, month_year):
+    month = db.session.query(Month).filter(Month.name == month_name).filter(Month.year == month_year).first()
+    return jsonify(month_schema.dump(month))
+
+
+@app.route("/reminder/add", methods=["POST"])
+def add_reminder():
+    if request.content_type != "application/json":
+        return jsonify("Error: Data must be sent as JSON.")
+
+    post_data = request.get_json()
+    text = post_data.get("text")
+    date = post_data.get("date")
+    month_id = post_data.get("month_id")
+
+    record = Reminder(text, date, month_id)
+    db.session.add(record)
+    db.session.commit()
+
+    return jsonify("Reminder added")
+
+@app.route("/reminder/get", methods=["GET"])
+def get_all_reminders():
+    all_reminders = db.session.query(Reminder).all()
+    return jsonify(multiple_reminder_schema.dump(all_reminders))
+
+@app.route("/reminder/get/<month_id>/<date>", methods=["GET"])
+def get_one_reminder(month_id, date):
+    reminder = db.session.query(Reminder).filter(Reminder.month_id == month_id).filter(Reminder.date == date).first()
+    return jsonify(reminder_schema.dump(reminder))
+
+@app.route("/reminder/update/<id>", methods=["PUT"])
+def update_reminder(id):
+    reminder = db.session.query(Reminder).filter(Reminder.id == id).first()
+    if reminder is None:
+        return jsonify(f"Error: No reminder with id {id}.")
+
+    put_data = request.get_json()
+    new_text = put_data.get("text")
+
+    if new_text == "":
+        return jsonify("Error: Text can not be blank.")
+
+    reminder.text = new_text
+    db.session.commit()
+
+    return jsonify("Reminder updated")
+
+@app.route("/reminder/delete/<id>", methods=["DELETE"])
+def delete_reminder(id):
+    reminder = db.session.query(Reminder).filter(Reminder.id == id).first()
+    db.session.delete(reminder)
+    db.session.commit()
+    return jsonify("Reminder deleted")
 
 
 if __name__ == "__main__":
